c6af7e0c531b28b7d3e04b511efad99d844ea267
Pablo Cruz <151473902+pablo-cruzbr@users.noreply.github.com>
2026-03-16 09:22:21 -0300

    feat(nav): apply dynamic background and shadow on scroll

--- components/Home/Nav.tsx/Nav.tsx
+++ components/Home/Nav.tsx/Nav.tsx
@@ -1,12 +1,30 @@
+"use client"
 import { NavLinks } from '@/constant/constant'
 import Link from 'next/link'
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import { BiCloudDownload } from 'react-icons/bi'
 import {FaCode} from "react-icons/fa"
+import { HiBars3BottomRight } from 'react-icons/hi2'
 
 const Nav = () => {
+    const [navBg, setNavBg] = useState(false)
+
+    useEffect(() => {
+        const handler = () => {
+            if (window.scrollY >= 90) setNavBg(true);
+            if (window.scrollY < 90) setNavBg(false);
+        };
+
+        window.addEventListener("scroll", handler);
+
+        return () => window. removeEventListener("scroll", handler);
+    }, []);
+
+
   return (
-    <nav className="fixed w-full h-[12vh] z-[100] bg-[#0C0D1F]/50 backdrop-blur-sm transition-all duration-200">
+    <nav className={`fixed w-full transition-all duration-200 h-[12vh] z-[100] flex items-center 
+            ${navBg ? "bg-[#0C0D1F] shadow-lg" : "bg-transparent"} 
+            ${!navBg && "backdrop-blur-sm bg-[#0C0D1F]/50"}`}>
         <div className="flex items-center h-full justify-between w-[90%] xl:w-[80%] mx-auto">
             <div className="flex items-center space-x-2">
                 <div className="w-10 h-10 bg-white rounded-full flex items-center justify-center">
@@ -34,6 +52,8 @@ const Nav = () => {
                   <BiCloudDownload className="w-5 h-5"/>
                   <span className="font-semibold">Download CV</span>
               </button>
+
+              <HiBars3BottomRight className="w-8 h-8 cursor-pointer text-whit lg:hidden" />
             </div>
         </div>
     </nav>
