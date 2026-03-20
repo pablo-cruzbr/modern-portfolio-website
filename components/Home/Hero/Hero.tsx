e6e6623b827a91165500144f1b0ea6e4014eb282
Pablo Cruz <pablo.cruz3360@gmail.com>
2026-03-19 22:59:19 -0300

    fix(mobile-nav): corrige posicionamento do ícone de fechar e animações

--- components/Home/Hero/Hero.tsx
+++ components/Home/Hero/Hero.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
-
+import Image from 'next/image'
 const Hero = () => {
   return (
-    <div className="relative h-screen flex items-center justify-center text-white overflow-hidden flex-col">Hero</div>
+    <div className="relative h-screen flex items-center justify-center text-white overflow-hidden flex-col">
+        <div className="relative z-10 flex-col items-center"></div>
+            
+            <h1 className="text-2xl sm:text-4x1 md:text-5xl lg:text-6x1 mt-6 text-center font-bold tracking-wide">Creating web products, brands an experienciences.</h1>
+    </div>
   )
 }
 
