8387f2b23b07ce09876c51546f7db04b2c0c94ed
Pablo Cruz <151473902+pablo-cruzbr@users.noreply.github.com>
2026-03-20 14:54:37 -0300

    style: adjust font size and spacing for Hero typewriter component

--- components/Home/Hero/Hero.tsx
+++ components/Home/Hero/Hero.tsx
@@ -1,5 +1,8 @@
-import React from 'react'
-import Image from 'next/image'
+"use client";
+import React from 'react';
+import Image from 'next/image';
+import Typewriter from 'typewriter-effect';
+
 const Hero = () => {
   return (
     <div className="relative h-screen flex items-center justify-center text-white overflow-hidden flex-col">
@@ -11,7 +14,27 @@ const Hero = () => {
                 height={150}
                 className='rounded-full border-8 border-[#0c0c48aa]'
             />
-              <h1 className="text-2xl sm:text-4x1 md:text-5xl lg:text-6x1 mt-6 text-center font-bold tracking-wide">Creating web products, <br/> brands, <span className="text-cyan-200">an experienciences.</span></h1> 
+              <h1 className="text-2xl sm:text-2xl md:text-5xl lg:text-6x1 mt-6 text-center font-bold tracking-wide">Creating web products, <br/> brands, <span className="text-cyan-200">an experienciences.</span></h1> 
+
+          <h2 className="mt-5 text-base px-2 text-center sm:text-2xl font-medium flex flex-wrap justify-center items-center">
+              Hi, I'm Pablo - A Passionate
+              <span className="text-cyan-200 font-bold ml-2"> 
+                <Typewriter
+                  options={{
+                      strings: [
+                        "Frontend Developer",
+                        "Backend Developer",
+                        "Fullstack Developer",
+                      ],
+                      autoStart: true,
+                      loop: true,
+                      delay: 75,
+                      deleteSpeed: 50,
+                      wrapperClassName: "pl-1" 
+                  }}
+                />
+              </span>
+            </h2>
     </div>
   )
 }
