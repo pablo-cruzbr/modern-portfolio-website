d2f48d100341a042df6574015c596a8bedd5d1de
Pablo Cruz <pablo.cruz3360@gmail.com>
2026-03-19 23:18:05 -0300

    feat: Installs typewriter-effect and refactors the Hero Section header.

--- components/Home/Hero/Hero.tsx
+++ components/Home/Hero/Hero.tsx
@@ -4,8 +4,14 @@ const Hero = () => {
   return (
     <div className="relative h-screen flex items-center justify-center text-white overflow-hidden flex-col">
         <div className="relative z-10 flex-col items-center"></div>
-            
-            <h1 className="text-2xl sm:text-4x1 md:text-5xl lg:text-6x1 mt-6 text-center font-bold tracking-wide">Creating web products, brands an experienciences.</h1>
+            <Image
+                src= "/img/s1.webp"
+                alt='heroimage'
+                width={150}
+                height={150}
+                className='rounded-full border-8 border-[#0c0c48aa]'
+            />
+              <h1 className="text-2xl sm:text-4x1 md:text-5xl lg:text-6x1 mt-6 text-center font-bold tracking-wide">Creating web products, <br/> brands, <span className="text-cyan-200">an experienciences.</span></h1> 
     </div>
   )
 }
