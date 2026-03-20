62f02a3deac3ea8d6791566bfa4e683da9335c23
Pablo Cruz <151473902+pablo-cruzbr@users.noreply.github.com>
2026-03-20 15:35:12 -0300

    feat: new section Services

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from './Hero/Hero'
+import Services from './Service/Services'
 
 const Home = () => {
   return (
     <div className="overflow-hidden">
       <Hero/>
+      <Services/>
     </div>
   )
 }
---
+++ components/Home/Service/Services.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Services = () => {
+  return (
+    <div className="pt-16 pb-16">
+        <h1 className="text-2xl sm:text-2xl md:text-5xl lg:text-6x1 mt-6 text-center font-bold tracking-wide">
+            Colleborate with brand <br/>
+            and agencies to create
+            <br/>impactful results
+        </h1>
+    </div>
+  )
+}
+
+export default Services
