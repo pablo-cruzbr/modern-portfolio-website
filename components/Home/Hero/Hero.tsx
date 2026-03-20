df4328f59207d9b094fc15978be8082955c45a03
Pablo Cruz <pablo.cruz3360@gmail.com>
2026-03-19 21:04:40 -0300

    feat(assets): upload image folder for site content

--- components/Home/Hero/Hero.tsx
+++ components/Home/Hero/Hero.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className="relative h-screen flex items-center justify-center text-white overflow-hidden flex-col">Hero</div>
   )
 }
 
