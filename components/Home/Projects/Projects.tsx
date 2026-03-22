e4d71e30c1b7804e3f5121205f29180e070aaddf
Pablo Cruz <pablo.cruz3360@gmail.com>
2026-03-21 22:30:49 -0300

    fix: syntax correction

--- components/Home/Projects/Projects.tsx
+++ components/Home/Projects/Projects.tsx
@@ -28,7 +28,7 @@ const Projects = () => {
                     height={650}
                     className="rounded-lg"
                 />
-                <h1 className="mt-4 text-xl sm:text-2xl font-semibold text-white">MPortfolio Website for Creatives</h1>
+                <h1 className="mt-4 text-xl sm:text-2xl font-semibold text-white">Portfolio Website for Creatives</h1>
                 <h1 className="pt-2 font-medium text-white">Bradning , Motion</h1>
             </div>
 
