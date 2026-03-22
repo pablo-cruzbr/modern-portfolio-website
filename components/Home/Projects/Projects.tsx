cbede3a623779d942cc663b27e634d31932fa3a4
Pablo Cruz <pablo.cruz3360@gmail.com>
2026-03-21 22:15:14 -0300

    styles: (ui) center text

--- components/Home/Projects/Projects.tsx
+++ components/Home/Projects/Projects.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Projects = () => {
   return (
-    <div>Projects</div>
+    <div className="pt-16 pb-16">
+        <h1 className="text-center text-3xl sm:text-4xl font-bold text-white">
+            A small selection of recent <span className="text-cyan-200"> projects</span>
+        </h1>
+    </div>
   )
 }
 
