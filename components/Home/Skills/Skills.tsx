9dea397b499b5ebfe2d48828a208ca7f4c8865f2
Pablo Cruz <pablo.cruz3360@gmail.com>
2026-03-22 14:04:25 -0300

    Título:feat(skills): implementa seção de habilidades com mapeamento de ícones

--- components/Home/Skills/Skills.tsx
+++ components/Home/Skills/Skills.tsx
@@ -1,5 +1,32 @@
 import React from 'react'
+import { SiJavascript, SiNextdotjs, SiNodedotjs, SiReact, SiTailwindcss, SiTypescript } from 'react-icons/si'
 
+const skills = [
+  {
+    name: "Javasript",
+    icon: <SiJavascript/>,
+    percentage: 89,
+  },
+
+  {
+    name: "React.js",
+    icon: <SiReact/>,
+    percentage: 89,
+  },
+
+  {
+    name: "Next.js",
+    icon: <SiNextdotjs/>,
+    percentage: 89,
+  },
+
+  {
+    name: "TypeScript",
+    icon: <SiTypescript/>,
+    percentage: 89,
+  },
+
+]
 const Skills = () => {
   return (
     <div>Skills</div>
