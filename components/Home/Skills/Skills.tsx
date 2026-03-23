83191548ea6dc77d44056f77929698dc9cc5c174
Pablo Cruz <151473902+pablo-cruzbr@users.noreply.github.com>
2026-03-23 11:07:37 -0300

    feat: implement interactive 3D tilt effect in Skills section

--- components/Home/Skills/Skills.tsx
+++ components/Home/Skills/Skills.tsx
@@ -1,5 +1,7 @@
+"use client"
 import React from 'react'
-import { SiJavascript, SiNextdotjs, SiNodedotjs, SiReact, SiTailwindcss, SiTypescript } from 'react-icons/si'
+import { SiJavascript, SiNextdotjs, SiNodedotjs, SiReact, SiTailwindcss, SiTypescript } from 'react-icons/si';
+import Tilt from "react-parallax-tilt";
 
 const skills = [
   {
@@ -29,8 +31,26 @@ const skills = [
 ]
 const Skills = () => {
   return (
-    <div>Skills</div>
-  )
-}
+    <div className="text-white pt-16 pb-16">
+      <h1 className="text-center text-2xl md:text-4xl xl:text-5xl font-bold text-white">
+        My <span className="text-cyan-300"> Skills</span>
+      </h1>
+
+      <div className="flex flex-wrap justify-center gap-6 mt-16">
+        {skills.map((skil) => {
+          return (
+            <Tilt key={skil.name} scale={1.1} transitionSpeed={400}>
+              <div className="bg-[#14134145] text-center w-40 h-48 rounded-3xl flex flex-col items-center justify-center shadow-lg transition hover:scale-105">
+                <div className="text-5xl mb-4 text-gray-300">{skil.icon}</div>
+                <p className="text-2xl font-semibold">{skil.percentage}</p>
+                <p className="text-cyan-300 mt-1">{skil.name}</p>
+              </div>
+            </Tilt>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
 export default Skills
